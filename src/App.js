1c989b70982aff9743fbdf68e60e293f511fb643
David Bui <tbui.vdev@gmail.com>
2021-11-05 17:25:35 -0500

    Add feature: double click to toggle reminder

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import Tasks from "./components/Tasks";
 import React, { useState } from "react";
 
 function App() {
-  const [tasks, setTasks] = useState([
+	const [tasks, setTasks] = useState([
 		{ id: 1, text: "Study HTML", day: "5/10/2021", reminder: true },
 		{ id: 2, text: "Study CSS", day: "6/11/2021", reminder: true },
 		{ id: 3, text: "Study JS", day: "7/13/2021", reminder: true },
 	]);
 
+	const canToggleReminder = id => {
+		setTasks(
+      tasks.map(task =>  task.id === id ? { ...task, reminder: !task.reminder } : task )
+    )
+	};
 
-  const canDeleteTask=(id)=> {
-    setTasks(tasks.filter(task => task.id !== id));
-  }
+	const canDeleteTask = id => {
+		setTasks(tasks.filter(task => task.id !== id));
+	};
 
-  return (
-    <div className = 'container'>
-      <Header />
-      <Tasks tasks={tasks} canDeleteTask={canDeleteTask}/>
-    </div>
-  );
+	return (
+		<div className="container">
+			<Header />
+			{tasks.length > 0 ? (
+				<Tasks
+					tasks={tasks}
+					canDeleteTask={canDeleteTask}
+					canToggleReminder={canToggleReminder}
+				/>
+			) : (
+				<p>You have nothing left to be done</p>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,11 @@
 
 import  { useState } from "react";
 import Task from "./Task";
-const Tasks = ({ tasks,canDeleteTask }) => {
+const Tasks = ({ tasks,canDeleteTask ,canToggleReminder}) => {
 	return (
 		<>
 			{tasks.map(task => (
-				<Task key={task.id} task={task} canDeleteTask={canDeleteTask} />
+				<Task key={task.id} task={task} canDeleteTask={canDeleteTask} canToggleReminder={canToggleReminder} />
 			))}
 		</>
 	);
