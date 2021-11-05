29e8f60012c35c04a851922f2142e074465ecb7e
David Bui <tbui.vdev@gmail.com>
2021-11-05 16:43:38 -0500

    Made delete button

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ function App() {
 		{ id: 3, text: "Study JS", day: "7/13/2021", reminder: true },
 	]);
 
+
+  const canDeleteTask=(id)=> {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
   return (
     <div className = 'container'>
       <Header />
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} canDeleteTask={canDeleteTask}/>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,11 @@
 
 import  { useState } from "react";
 import Task from "./Task";
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks,canDeleteTask }) => {
 	return (
 		<>
 			{tasks.map(task => (
-				<Task key={task.id} task={task} />
+				<Task key={task.id} task={task} canDeleteTask={canDeleteTask} />
 			))}
 		</>
 	);
